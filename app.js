48054b53144d1f76a7377733992ae674e1244357
Karthick <mkartik17@gmail.com>
2016-11-18 18:07:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,79 @@
-var port=process.env.PORT || 3000;
-var http=require('http');
-var app=http.createServer(function(req,res){
-    res.write("server listening to port:"+port);
-    res.end();
-}).listen(port);
-socket=require("socket.io");
-io=socket.listen(app);
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+
+var app = express();
+//var server = app.listen(3000);
+// START SERVER
+var server = app.listen(3000, "0.0.0.0", function () {
+  var host = server.address().address
+  var port = server.address().port
+ // var headers = server.address().header;
+  console.log("kitrak app listening at http://%s:%s", host, port)
+})
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(room, msg){
+    io.emit(room, msg);
+  });
 });
-io.sockets.on("connection",function(socket){
-    console.log("new connection");
-    socket.on("eventA",function(data){
-        io.sockets.emit("eventB",data);
-    }); 
+
+var routes = require('./routes/index');
+//var users = require('./routes/users');
+
+app.use(expressValidator());
+
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.set('views', './views');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+//app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
 module.exports = app;
