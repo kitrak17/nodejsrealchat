6de3296c3a982001d2d072d2d4cd375de0a49c49
Karthick <karthickm@wekancode.com>
2016-11-18 14:25:22 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/*router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});*/
+
+router.get('/room1', function(req, res){
+  res.sendfile('views/room1.html');
+});
+router.get('/room2', function(req, res){
+  res.sendfile('views/room2.html');
+});
+
+module.exports = router;
